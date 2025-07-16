e606fe995144717c8af642a0d1f801fece29df35
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-07-16 23:27:14 +0530

    printf() method practise

---
+++ C1.java
@@ -0,0 +1,13 @@
+class C1{
+	public static void main(String args[]){
+		int id= 1;
+		String name = "ishara";
+		double salary=123.34;
+		boolean status= true;
+		System.out.printf("%-8s:%-20s:%-15s:%-10s","id", "name", "salary", "status");
+		System.out.println("");
+		System.out.printf("%-8s:%-20s:%-15s:%-10s",id, name, salary, true);
+		System.out.println("");
+		System.out.println("============================================================");
+	}
+}
---
+++ C2.java
@@ -0,0 +1,11 @@
+class C2{
+	public static void main(String args[]){
+		int id= 1;
+		String name = "ishara";
+		double salary=123.34;
+		boolean status;
+		System.out.printf("%-8s %-20s %-25s %-15s","id: "+id, "name: "+name, "salary: "+salary, "status: "+true);
+		System.out.println("");
+		System.out.println("============================================================");
+	}
+}
